2bd36b4664927229767385f5547087d069c98480
Kevin <894784649@qq.com>
2023-12-08 10:42:22 +0800

    perf: NO big.Float and big.Int

--- utils/money/calculate.go
+++ utils/money/calculate.go
@@ -4,27 +4,22 @@ import (
 	"github.com/zeromicro/go-zero/core/errorx"
 	"github.com/zeromicro/go-zero/core/logx"
 	"math"
-	"math/big"
 )
 
 // CalculateRatePriceInternal 计算百分比金额的内部函数
 func CalculateRatePriceInternal(price int32, rate float64) (int32, error) {
-	priceBigInt := big.NewFloat(float64(price))
-
-	rateFloat := new(big.Float).SetFloat64(rate)
-
-	resultFloat := new(big.Float)
-	resultFloat.Mul(rateFloat, priceBigInt)
-	resultFloat.Quo(resultFloat, big.NewFloat(100))
-
-	result := new(big.Int)
-	resultFloat.Int(result)
-	int64Value := result.Int64()
-	if int64Value > math.MaxInt32 {
-		logx.Error("big.Int value out of int32 range")
-		return 0, errorx.NewInvalidArgumentError("big.Int value out of int32 range")
+	if rate == 0 {
+		return 0, nil
 	}
-	int32Value := int32(int64Value)
+	if rate == 100 {
+		return price, nil
+	}
 
-	return int32Value, nil
+	result := float64(price) * rate / 100
+	if result > float64(math.MaxInt32) {
+		logx.Error("Result out of int32 range")
+		return 0, errorx.NewInvalidArgumentError("Result out of int32 range")
+	}
+
+	return int32(result), nil
 }
